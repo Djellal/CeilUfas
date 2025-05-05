50cc058c8be941e3b6019cb8ca7a2cac380967d0
Djellal Elkacem <djellal.elkacem@gmail.com>
2025-05-05 18:56:37 +0100

    feat: add Globals class and enhance profession page UI
    Add a new Globals class to define roles and improve the profession page UI with better styling and layout. Also, update Program.cs to seed roles and an admin user on application startup.

---
+++ Globals.cs
@@ -0,0 +1,11 @@
+namespace Ceilufas
+{
+    public static class Globals
+    {
+        public const string ADMIN = "ADMIN";
+        public const string STUDENT = "STUDENT";
+        public const string TEACHER = "TEACHER";
+
+        public static readonly string[] Roles = { ADMIN, STUDENT, TEACHER };
+    }
+}
